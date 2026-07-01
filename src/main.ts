5fd2ee494533a4d2d87b217121c7a1411027b472
yaroslav <slavik77rad@gmail.com>
2026-07-01 13:31:22 +0300

    added analytics

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { inject } from '@vercel/analytics'
 import App from './App.vue'
 import router from './router'
 import { i18n } from './i18n'
+
+inject()
 
 createApp(App).use(createPinia()).use(router).use(i18n).mount('#app')
